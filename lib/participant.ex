417864f31be2da3c9f11cbf2ea277bb738c1ff92
Anand Mani <cma.camelot@gmail.com>
2018-09-27 21:18:13 -0400

    Moved Agent to GenServer. Still suffering from timeout

--- lib/participant.ex
+++ lib/participant.ex
@@ -1,52 +1,137 @@
 defmodule Participant do
-  use Agent
+  use GenServer
 
-  def start_link(_opts) do
-    Agent.start_link(fn -> %{
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def learnNeighbours(participant, neighbours) do
+    GenServer.call(participant, {:learnNeighbours, {neighbours}})
+  end
+
+  def inspect(participant) do
+    GenServer.call(participant, {:inspect, {}})
+  end
+
+  def receiveRumour(participant, rumour) do
+    GenServer.call(participant, {:receiveRumour, {rumour}})
+  end
+
+  def sendRumour(participant, rumour) do
+    GenServer.call(participant, {:sendRumour, {rumour}})
+  end
+
+  #Server APIs
+  def init(:ok) do
+    {:ok, %{
       :neighbours => {},
       :rumour => %{
         :text => nil,
         :count => 0
       }
-    } end)
-    #Send pid to GenServer
+    }}
   end
 
-  def learnNeighbours(participant, neighbours) do
-    # IO.write("learnNeighbours "); IO.inspect(participant); IO.inspect(neighbours)
-    Agent.update(participant, fn state -> 
-      put_in state.neighbours, neighbours
-    end)
+
+  def handleReceiveRumour(rumour, state) do
+    cond do
+      state.rumour.text == rumour -> put_in state.rumour.count, state.rumour.count + 1
+      state.rumour.text == nil -> put_in state.rumour, %{:text => rumour, :count => 1} 
+      true -> raise "Invalid rumour"
+    end
   end
 
-  # def inspect(participant) do 
-  #   Agent.get(participant, fn state -> IO.inspect state end)
-  # end
-
-  def receiveRumour(participant, rumour) do
-    IO.write "Rumour received by "; IO.inspect participant    
-    Agent.update(participant, fn state ->
-     cond do
-       state.rumour.text == rumour -> put_in state.rumour.count, state.rumour.count + 1
-       state.rumour.text == nil -> put_in state.rumour, %{:text => rumour, :count => 1} 
-       true -> raise "Invalid rumour"
-     end
-    end)
-    sendRumour(participant)
+  def handleSendRumour(state) do
+    cond do
+      state.rumour.count < 10 ->
+        numNeighbours = tuple_size state.neighbours
+        if(numNeighbours == 0) do raise "No neighbours" end
+        randomNeighbour = elem(state.neighbours, :rand.uniform(numNeighbours)-1)
+        IO.write "Sending rumour to"; IO.inspect randomNeighbour
+        receiveRumour(randomNeighbour, state.rumour.text)
+      true -> IO.puts("finish")
+        #Terminate program
+    end
   end
 
-  def sendRumour(participant) do
-    Agent.get(participant, fn state ->
-      cond do
-        state.rumour.count < 10 ->
-          numNeighbours = tuple_size state.neighbours
-          if(numNeighbours == 0) do raise "No neighbours" end
-          randomNeighbour = elem(state.neighbours, :rand.uniform(numNeighbours)-1)
-          IO.write "Sending rumour to"; IO.inspect randomNeighbour
-          receiveRumour(randomNeighbour, state.rumour.text)
-        true -> IO.puts("finish")
-          #Terminate program
-      end
-    end)
+  def handle_call(arg1, _from, state) do
+    {method, methodArgs} = arg1
+    # IO.write "from"; IO.inspect _from
+    case method do
+      :learnNeighbours -> 
+        {neighbours} = methodArgs
+        newState = put_in state.neighbours, neighbours
+        {:reply, :ok, newState}
+      :inspect -> 
+        {:reply, state, state}
+      :receiveRumour ->
+        {rumour} = methodArgs
+        newState = handleReceiveRumour(rumour, state)
+        {pid, ref} = _from
+        sendRumour(pid, rumour)
+        {:reply, :ok, newState}
+      :sendRumour ->
+        {rumour} = methodArgs
+        newState = handleSendRumour(state)
+        {:reply, :ok, state}
+    end
   end
+
 end
+
+
+
+
+
+# defmodule Participant do
+#   use Agent
+
+#   def start_link(_opts) do
+#     Agent.start_link(fn -> %{
+#       :neighbours => {},
+#       :rumour => %{
+#         :text => nil,
+#         :count => 0
+#       }
+#     } end)
+#     #Send pid to GenServer
+#   end
+
+#   def learnNeighbours(participant, neighbours) do
+#     # IO.write("learnNeighbours "); IO.inspect(participant); IO.inspect(neighbours)
+#     Agent.update(participant, fn state -> 
+#       put_in state.neighbours, neighbours
+#     end)
+#   end
+
+#   # def inspect(participant) do 
+#   #   Agent.get(participant, fn state -> IO.inspect state end)
+#   # end
+
+#   def receiveRumour(participant, rumour) do
+#     IO.write "Rumour received by "; IO.inspect participant    
+#     Agent.update(participant, fn state ->
+#      cond do
+#        state.rumour.text == rumour -> put_in state.rumour.count, state.rumour.count + 1
+#        state.rumour.text == nil -> put_in state.rumour, %{:text => rumour, :count => 1} 
+#        true -> raise "Invalid rumour"
+#      end
+#     end)
+#     sendRumour(participant)
+#   end
+
+#   def sendRumour(participant) do
+#     Agent.get(participant, fn state ->
+#       cond do
+#         state.rumour.count < 10 ->
+#           numNeighbours = tuple_size state.neighbours
+#           if(numNeighbours == 0) do raise "No neighbours" end
+#           randomNeighbour = elem(state.neighbours, :rand.uniform(numNeighbours)-1)
+#           IO.write "Sending rumour to"; IO.inspect randomNeighbour
+#           receiveRumour(randomNeighbour, state.rumour.text)
+#         true -> IO.puts("finish")
+#           #Terminate program
+#       end
+#     end)
+#   end
+# end
--- lib/two.ex
+++ lib/two.ex
@@ -1,3 +1,51 @@
+# defmodule TWO do
+
+#   def generateParticipants(numNodes, topology, algorithm) do
+#     # IO.puts("Generating Participants..")
+#     participants = 1..numNodes
+#       |> Enum.map(fn index -> 
+#           {:ok, pid} = Participant.start_link([])
+#           {index, pid}
+#          end) 
+#       |> Map.new
+#     # IO.inspect(participants)
+#     neighbours = participants
+#       |> Enum.map(fn {index, pid} -> 
+#           nodeNeighbours = if(participants[index-1] != nil) do
+#             {participants[index-1]}
+#           end || {}
+#           nodeNeighbours = if(participants[index+1] != nil) do
+#             Tuple.append(nodeNeighbours, participants[index+1])
+#           end || nodeNeighbours
+#           {pid, nodeNeighbours}
+#          end)
+#       |> Map.new
+#     # IO.inspect(neighbours)
+#     neighbours 
+#     |> Enum.map(fn {pid, neighbours} -> Participant.learnNeighbours(pid, neighbours) end)
+
+#     %{:participants => participants, :neighbours => neighbours}
+#     # participants
+#     # |> Enum.map(fn {index, pid} -> Participant.inspect(pid) end)    
+#   end
+
+#   def startRumour(rumour, state) do
+#     # IO.write "startRumour1"; IO.inspect(state)
+#     Participant.receiveRumour(state.participants[1], rumour)
+#   end
+
+#   def start(numNodes, topology, algorithm) do
+#     generateParticipants(numNodes, topology, algorithm)
+#     startRumour("Hello World")
+#   end
+  
+# end
+
+
+
+
+
+
 defmodule TWO do
   use GenServer
 
