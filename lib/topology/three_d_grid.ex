9d195de00b9fb71bf3a3549726eedfcc3142f113
Anand Mani <cma.camelot@gmail.com>
2018-09-29 00:37:26 -0400

    added neighbours for 3D

--- lib/topology/three_d_grid.ex
+++ lib/topology/three_d_grid.ex
@@ -1,19 +1,43 @@
 defmodule ThreeDGrid do
+  def get_3D_neighbor(actor_index, a) do
+    x = Float.ceil(RC.nth_root(3, Map.size(actor_index)))
+    x = Kernel.trunc(x)
+    y = elem(a, 0)
+
+    neighbors =
+      cond do
+        rem(y, x) == 0 ->
+          [Map.get(actor_index, y + 1)]
+
+        rem(y, x) == x - 1 ->
+          [Map.get(actor_index, y - 1)]
+
+        true ->
+          [Map.get(actor_index, y + 1)] ++ [Map.get(actor_index, y - 1)]
+      end
+
+    neighbors =
+      neighbors ++
+        cond do
+          rem(y, x * x) >= 0 && rem(y, x * x) < x ->
+            [Map.get(actor_index, y + x)]
+
+          rem(y, x * x) >= x * x - x && rem(y, x * x) < x * x ->
+            [Map.get(actor_index, y - x)]
+
+          true ->
+            [Map.get(actor_index, y + x)] ++ [Map.get(actor_index, y - x)]
+        end
+
+    neighbors =
+      neighbors ++ [Map.get(actor_index, y + x * x)] ++ [Map.get(actor_index, y - x * x)]
+
+    List.to_tuple(MyList.delete_all(neighbors, nil))
+  end
+
   def findNeighbours(participants) do
-    participants
-    |> Enum.map(fn {index, pid} ->
-      nodeNeighbours =
-        if(participants[index - 1] != nil) do
-          {participants[index - 1]}
-        end || {}
-
-      nodeNeighbours =
-        if(participants[index + 1] != nil) do
-          Tuple.append(nodeNeighbours, participants[index + 1])
-        end || nodeNeighbours
-
-      {pid, nodeNeighbours}
-    end)
-    |> Map.new()
+    for a <- participants, id = elem(a, 1), data = get_3D_neighbor(participants, a), into: %{} do
+      {id, data}
+    end
   end
 end
